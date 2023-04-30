79187401f5a4d0e0668e775a2abe8b84d8546e67
Zachary Soliman <zachsoliman1@gmail.com>
2023-04-30 13:39:14 -0400

    background click closes modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,12 @@
 const Modal = ({ art, isHidden, displayModal }) => {
     return (
         <div className={`${isHidden ? "hidden" : ""}`}>
-            <div className="modalBackground">
+            <div
+                className="modalBackground"
+                onClick={displayModal}>
                 <div className="modal">
                     {/* <img src={art} /> */}
                     {/* {console.log("art", art)} */}
-                    <button onClick={displayModal}>x</button>
                     <div className="ImagePlaceholder"></div>
                 </div>
             </div>
